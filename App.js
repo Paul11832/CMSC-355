92ea911a128047643bbac7bb448da2fa73b68f7c
Paul11832 <111941735+Paul11832@users.noreply.github.com>
2024-03-28 22:17:10 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
-
+// Filename - App.js
+ 
+import "./App.css";
+// importing components from react-router-dom package
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Navigate,
+} from "react-router-dom";
+ 
+import { useForm } from "react-hook-form";
+// import Home component
+import Home from "./components/Home";
+// import About component
+import Grades from "./components/Grades";
+// import ContactUs component
+import Lunch from "./components/Lunch";
+import Login from "./components/Login";
+import Librarary from "./components/Librarary";
+ 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return(
+    <>
+            {/* This is the alias of BrowserRouter i.e. Router */}
+            <Router>
+                <Routes>
+                    {/* This route is for home component 
+          with exact path "/", in component props 
+          we passes the imported component*/}
+          <Route
+          exact 
+          path = "/"
+          element = {<Login/>}
+          />
+                    <Route
+                        path="/Home"
+                        element={<Home />}
+                    />
+ 
+                    {/* This route is for about component 
+          with exact path "/about", in component 
+          props we passes the imported component*/}
+                    <Route
+                        path="/Lunch"
+                        element={<Lunch />}
+                    />
+ 
+                    {/* This route is for contactus component
+          with exact path "/contactus", in 
+          component props we passes the imported component*/}
+                    <Route
+                        path="/Grades"
+                        element={<Grades />}
+                    />
+                    <Route
+                        path="/Librarary"
+                        element={<Librarary />}
+                    />
+                    {/* If any route mismatches the upper 
+          route endpoints then, redirect triggers 
+          and redirects app to home component with to="/" */}
+                    {/* <Redirect to="/" /> */}
+                    <Route
+                        path="*"
+                        element={<Navigate to="/" />}
+                    />
+                </Routes>
+            </Router>
+        </>
+    );
 }
-
+ 
 export default App;
