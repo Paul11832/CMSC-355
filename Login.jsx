c0f53e3227061b2373a438fbf14241c35b0f1c7c
Paul11832 <111941735+Paul11832@users.noreply.github.com>
2024-03-31 11:13:50 -0400

    Update Login.jsx
    IT WORKS

--- Login.jsx
+++ Login.jsx
@@ -3,29 +3,47 @@
 import React from "react";
 import { useForm } from "react-hook-form";
 import {useState } from "react";
+import { Navigate, useNavigate } from 'react-router-dom';
 //import { Link } from "react-router-dom";
 import {Route, Link, Routes, useParams} from 'react-router-dom';
 
+
 function Login() {
-    var params = useParams();
-    console.log(params);
+    const navigate = useNavigate();
+    const[notloggedIn,setLoggedIn] = useState(true);
+    const[currUsername,setState] = useState('')
+    const[currPassword,setPass] = useState('')
+    const preformLogin = () =>{
+        if(currUsername === "Paul" && currPassword ==="Pass"){
+            setLoggedIn(false)
+        navigate('/Home')
+        }else{
+            
+        }
+    }
   
     return(
     <>
      <div className="App">
             <h1>Login Page</h1>
-            <form>
+            <form onSubmit={preformLogin}>
                 <label htmlFor="Student Name">Student Name:</label>
-                <input type="text" id="Student Name" name="Student Name" />
+                <input type={'text'} 
+                id={'Student Name'} 
+                name={'Student Name'}
+                onChange={event =>{
+                    setState(event.target.value)
+                }} />
                 <label htmlFor="Student ID">Student ID:</label>
-                <input type="password" id="password" name="password"  />
-
+                <input type="password" 
+                id="password" 
+                name="Student ID"  
+                onChange={event =>{
+                    setPass(event.target.value)
+                }}/>
                 <button type="submit">Login</button>
+                <span> {"\n"}'You are not logged in'</span>
                 <ul>
-        <li>
-        {/* Endpoint to route to Home component */}
-        <Link to="/Home">Home</Link>
-    </li>
     </ul>
             </form>
         </div>
