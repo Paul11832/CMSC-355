00637f09e74e68704466de9dea51c0b9bc6c5264
Paul11832 <111941735+Paul11832@users.noreply.github.com>
2024-04-01 11:29:23 -0400

    Update Grades.jsx
    go home

--- Grades.jsx
+++ Grades.jsx
@@ -1,11 +1,16 @@
-
+// App.jsx
  
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
  
 const Grades = () => {
     return (
         <div>
             <h1>Grades</h1>
+            <li>
+                    {/* Endpoint to route to Home component */}
+                    <Link to="/Home">Home</Link>
+                </li>
         </div>
     );
 };
