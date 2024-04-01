e0c68f0971c23745a116f07f5d4844eda94e2fbe
Paul11832 <111941735+Paul11832@users.noreply.github.com>
2024-04-01 11:29:46 -0400

    Update Librarary.jsx
    go home

--- Librarary.jsx
+++ Librarary.jsx
@@ -1,11 +1,15 @@
 
- 
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
  
 const Librarary = () => {
     return (
         <div>
-            <h1>About Page</h1>
+            <h1>Library</h1>
+            <li>
+                    {/* Endpoint to route to Home component */}
+                    <Link to="/Home">Home</Link>
+                </li>
         </div>
     );
 };
