45c5996984490dfa1f12a70f695f2b048727b558
Paul11832 <111941735+Paul11832@users.noreply.github.com>
2024-04-24 23:52:56 -0400

    Update Home.jsx

--- Home.jsx
+++ Home.jsx
@@ -3,10 +3,23 @@ import React from "react";
 // Importing Link from react-router-dom to 
 // navigate to different end points.
 import { Link } from "react-router-dom";
- 
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { useCallback } from 'react';
+import Calendar from "react-calendar";
+
 const Home = () => {
     const name = localStorage.getItem("Username");
-    console.log(name);
+    const [items, setItems] = useState([]);
+    const [data,setData] = useState([]);
+    const[day,setDay]= useState(false);
+    const[month,setMonth]= useState(false);
+    useEffect(() => {
+  localStorage.setItem(120, JSON.stringify(items));
+}, [items]);
+const handleEvent1 = () =>{
+    setDay(true);
+}
     return (
         <div>
             <h1>Home Page</h1>
@@ -26,6 +39,27 @@ const Home = () => {
                     <Link to="/Librarary">Librarary</Link>
                 </li>
             </ul>
+            <Calendar
+            onChange={setData}
+            selectRange = {true}
+            defaultValue={data}
+            onClickDay={() =>setDay(!day)}
+            onClickMonth={() =>setMonth(!month)}
+            ></Calendar>
+            {day && ( 
+                <div>
+                    <li>Classes end Tuesday April 30th!</li>
+                    <li>Final exam will be help May 2nd</li>
+                    <li>All grades are due May 6th</li>
+                    </div>
+            )}
+            {month && ( 
+                <div>
+                    <li>Welcome to April!</li>
+                    <li>Last month is upon us!</li>
+                    <li>Next month is finals season </li>
+                    </div>
+            )}
         </div>
     );
 };
